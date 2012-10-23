99267711d1f5edb4b1e6b8537364432835c398b3
Qian LinFeng <thewawar@gmail.com>
2012-10-23 10:23:35 +0800

    fix

--- manager.py
+++ manager.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 #coding=utf-8
 
+import sys
 from myway import create_app
-from myway.utils import db
+
+def create_all():
+    from myway.utils import db
+    db.create_all()
+    print 'DB created!'
+
+func = {}
+func['create_all'] = create_all
 
 app = create_app('settings.py')
-
 if __name__ == '__main__':
-    # db.create_all()
+    print globals()
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+        args = sys.argv[2:]
+        apply(func[name], args)
+    
     app.run(host='0.0.0.0', port=1989)
