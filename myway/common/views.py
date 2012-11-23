f31deccbb6cdd2c6cf2e202da5d7b7b38afcf52a
TheWaWaR <thewawar@gmail.com>
2012-11-23 21:46:39 +0800

    topo Change names

--- myway/common/views.py
+++ myway/common/views.py
@@ -54,7 +54,7 @@ def topo():
 def json_load_directory():
     spath = request.args.get('path', '')
     data = None
-    lvs_all = ['ROOT', 'OLT', 'ONU', 'EOC', 'CPE']
+    lvs_all = ['ROOT', 'STAR', 'COUNTRY', 'PROVINCE', 'CITY']
     
     if spath:
         data = []
@@ -81,10 +81,10 @@ def json_load_directory():
         }
         for i in range(1, 100):
             node = {
-                'name'     : 'OLT-' + str(i),
+                'name'     : 'STAR-' + str(i),
                 'children' : [],
                 'level'    : 1,
-                'id'       : 'olt-' + str(i)
+                'id'       : 'star-' + str(i)
             }
             data['children'].append(node)
     return json.dumps(data)
@@ -101,13 +101,13 @@ def json_load_nodes():
     from random import Random
     rand = Random()
     
-    spath = request.args.get('path', 'olt-0')
+    spath = request.args.get('path', 'star-0')
     na    = request.args.get('na', 7, type=int)
     nb    = request.args.get('nb', 7, type=int)
     nnc   = request.args.get('nc', 7, type=int)
 
     ca = cb = cc = 0;
-    onu_lv, eoc_lv, cpe_lv = 2, 3, 4
+    country_lv, province_lv, city_lv = 2, 3, 4
     path = spath.split(',')[1:]
     lvs = [i.split('-')[0].upper() for i in path]
     
@@ -128,32 +128,32 @@ def json_load_nodes():
     print spath, path, lvs
     print '--------------------'
     
-    for a in range(na):         # ONU
+    for a in range(na):         # COUNTRY
         ca = a+1
-        A = {'name': 'ONU-' + str(ca), 'children': [],
-             'level': onu_lv, 'id': 'onu-'+str(ca), "size": ca * 30}
-        for b in range(nb):     # EOC
+        A = {'name': 'COUNTRY-' + str(ca), 'children': [],
+             'level': country_lv, 'id': 'country-'+str(ca), "size": ca * 30}
+        for b in range(nb):     # PROVINCE
             cb = b+1
-            B = {'name': 'EOC-' + str(cb), 'children': [],
-                 'level': eoc_lv, 'id': 'eoc-'+str(cb), "size": cb * 30}
+            B = {'name': 'PROVINCE-' + str(cb), 'children': [],
+                 'level': province_lv, 'id': 'province-'+str(cb), "size": cb * 30}
             nc = nnc
-            for c in range(nc): # CPE
+            for c in range(nc): # CITY
                 cc = c+1
-                C = {'name': 'CPE-' + str(cc), 'url': 'http://www.stackoverflow.com',
-                     'level': cpe_lv, 'id': 'cpe-'+str(cc) , "size": cc * 30}
+                C = {'name': 'CITY-' + str(cc), 'url': 'http://www.stackoverflow.com',
+                     'level': city_lv, 'id': 'city-'+str(cc) , "size": cc * 30}
                 C['status'] = 1 if c % rand.randint(2, 6) != 0 else 0
                 C['lstatus'] =  1 if c % 5 > 1 else 0
-                if selected('CPE', cc):
-                    if data['maxlevel'] < cpe_lv:
-                        data['maxlevel'] = cpe_lv;
+                if selected('CITY', cc):
+                    if data['maxlevel'] < city_lv:
+                        data['maxlevel'] = city_lv;
                     B['children'].append(C)
-            if selected('EOC', cb):
-                if data['maxlevel'] < eoc_lv:
-                    data['maxlevel'] = eoc_lv;
+            if selected('PROVINCE', cb):
+                if data['maxlevel'] < province_lv:
+                    data['maxlevel'] = province_lv;
                 A['children'].append(B)
-        if selected('ONU', ca):
-            if data['maxlevel'] < onu_lv:
-                data['maxlevel'] = onu_lv;
+        if selected('COUNTRY', ca):
+            if data['maxlevel'] < country_lv:
+                data['maxlevel'] = country_lv;
             data['children'].append(A)
 
     def pdict(d):
